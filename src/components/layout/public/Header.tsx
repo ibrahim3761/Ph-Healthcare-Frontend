e5cb5da653382d04a6dfb00daff03a3f8ca925eb
ibrahim3761 <mdibrahimabdullah1@gmail.com>
2026-09-16 13:03:19 +0600

    feat: add user registration and logout functionality; implement user retrieval and Google OAuth hooks; create RegistrationPayload type

--- src/components/layout/public/Header.tsx
+++ src/components/layout/public/Header.tsx
@@ -1,12 +1,43 @@
+"use client";
+import { userLogout } from "@/api";
 import Logo from "@/assets/svg/Logo";
 import { Button } from "@/components/ui/button";
+import { toast } from "@/components/ui/toast";
+import { useGetMe, useLogout } from "@/hooks/auth.hook";
+import { useQueryClient } from "@tanstack/react-query";
 import Link from "next/link";
 
 export default function Header() {
+
   const routes = [
     { name: "Home", url: "/" },
     { name: "About us", url: "/about-us" },
   ];
+
+  const {data, isLoading} = useGetMe();
+  const {mutate: logout} = useLogout();
+  const queryClient = useQueryClient();
+
+  const handleLogout = () => {
+    logout(undefined, {
+      onSuccess:()=>{
+        toast.add({
+          title: "Logout Success",
+          description: "You have been logged out",
+          type: "success",
+        });
+        queryClient.removeQueries({queryKey: ["user"]});
+      },
+      onError: (err) => {
+        toast.add({
+          title: "Logout failure",
+          description: err.message || "Something went wrong. Please try again",
+          type: "error",
+        });
+      }
+    })
+  }
+  
 
   return (
     <header className="w-full h-16 border border-b">
@@ -23,13 +54,22 @@ export default function Header() {
           ))}
         </nav>
         <div>
-          <Button
-            variant="outline"
-            render={<Link href="/login">Login</Link>}
-            nativeButton={false}
-          >
-            login
-          </Button>
+          {!isLoading && !data && (
+            <Button
+              variant="outline"
+              render={<Link href="/login">Login</Link>}
+              nativeButton={false}
+            >
+            Login
+          </Button>)}
+          {!isLoading && data && (
+            <Button
+              variant="destructive"
+              onClick={handleLogout}
+              
+            >
+            Logout
+          </Button>)}
         </div>
       </div>
     </header>
--- src/hooks/auth.hook.ts
+++ src/hooks/auth.hook.ts
@@ -1,8 +1,34 @@
-import { userLogin } from "@/api";
-import { useMutation } from "@tanstack/react-query";
+import { getMe, googleOAuth, userLogin, userLogout, userRegistration } from "@/api";
+import { useMutation, useQuery } from "@tanstack/react-query";
 
 export function useLogin() {
   return useMutation({
     mutationFn: userLogin,
+  });
+}
+
+export function useRegistration() {
+  return useMutation({
+    mutationFn: userRegistration,
+  });
+}
+
+export function useLogout() {
+  return useMutation({
+    mutationFn: userLogout,
+  });
+}
+
+export function useGoogleOAuth() {
+  return useMutation({
+    mutationFn: googleOAuth,
+  });
+}
+
+export function useGetMe() {
+  return useQuery({
+    queryKey: ["user"],
+    queryFn: getMe,
+    retry: false,
   });
 }
