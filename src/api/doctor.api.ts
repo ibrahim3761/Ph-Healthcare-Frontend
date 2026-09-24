484a11411f6ed973a90e01f1b899b4ddcf6a9d8c
ibrahim3761 <mdibrahimabdullah1@gmail.com>
2026-09-24 13:33:03 +0600

    feat: implement doctor approval workflow with loading state, review actions, and API integration

--- src/api/doctor.api.ts
+++ src/api/doctor.api.ts
@@ -1,6 +1,5 @@
 import apiClient from "@/lib/apiClient";
-import { VerifyAccountPayload } from "@/types";
-import { DoctorApplicationPayload } from "@/types/doctor.type";
+import { ApiResponse, ApproveDoctorPayload, Doctor, DoctorApplicationPayload, DoctorParams, VerifyAccountPayload } from "@/types";
 
 export function applyAsDoctor(payload: DoctorApplicationPayload) {
   const formData = new FormData();
@@ -20,6 +19,19 @@ export function applyAsDoctor(payload: DoctorApplicationPayload) {
 
 export function verifyDoctorAccount(payload: VerifyAccountPayload) {
   return apiClient("/doctor/apply-as-doctor/verify-email", {
+    method: "POST",
+    body: payload,
+  });
+}
+
+export function getAllDoctors(params: DoctorParams) {
+  return apiClient<ApiResponse<Doctor[]>>("/doctor/all-doctors", {
+    params,
+  });
+}
+
+export function approveDoctor(payload: ApproveDoctorPayload) {
+  return apiClient("/doctor/approve-doctor", {
     method: "POST",
     body: payload,
   });
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,4 @@ export * from "./auth.type";
 export * from "./doctor.type";
 export * from "./user.type";
 export * from "./sidebar.type";
+export * from "./api.type";
